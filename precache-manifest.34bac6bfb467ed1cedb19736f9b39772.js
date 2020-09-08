011de24198d664467673934a7ff1d321cde44c10
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-09-07 18:52:00 -0600

    Updates

---
+++ precache-manifest.34bac6bfb467ed1cedb19736f9b39772.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156ac3a361a81b7f6e38606f2f831bf7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c0c7d838d51fe242d57e",
+    "url": "/portfolio/static/css/main.bb9549d7.chunk.css"
+  },
+  {
+    "revision": "348e332a2338282d3e90",
+    "url": "/portfolio/static/js/2.97644493.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.97644493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c7d838d51fe242d57e",
+    "url": "/portfolio/static/js/main.d022857c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6b868382c775db5f343ab0e49b414b7d",
+    "url": "/portfolio/static/media/Untitled.6b868382.png"
+  },
+  {
+    "revision": "bad31abb131b8362bbbd1f95b633a8da",
+    "url": "/portfolio/static/media/background.bad31abb.jpg"
+  },
+  {
+    "revision": "3b147600c53bae3602e367d4d06d7843",
+    "url": "/portfolio/static/media/foodTruck.3b147600.png"
+  },
+  {
+    "revision": "20dadde100984a690e2f5f7d1890a55b",
+    "url": "/portfolio/static/media/pat.20dadde1.png"
+  }
+]);
