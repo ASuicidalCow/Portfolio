7536c1a49ef6e4d6df1d8b69b935a407c2cd850d
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-09-07 18:57:13 -0600

    added about and deployed to gh-pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel'
 import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
 
 class Home extends Component {
   render() {
@@ -9,6 +10,7 @@ class Home extends Component {
       <>
         <Navbar></Navbar>
         <Carousel></Carousel>
+        <AboutMe></AboutMe>
         <Footer></Footer>
       </>
     );
