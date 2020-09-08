07f5cf4292691ee8b3776e487c972e25d8e38f70
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-09-07 18:57:33 -0600

    added to the about me

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export class AboutMe extends React.Component{
+
+  render() {
+    return (
+      <section>
+        <div className='container'>
+          <h1 className='container-fluid mb-5 mt-5 text-center'>About ME</h1>
+          <h2 className='mt-4'> I grew up in little Casper, Wyoming where I lived for 17 years before moving to my current home state of Utah with my parents. I then started what I thought was going to be my career for life at FedEx. I had realized that this was not the career I wanted. I searched and went to different colleges to find something that fit me and nothing quite stuck until I found computer programming where I discovered a love of problem solving and creating things that others might possible find useful or that I find useful for my self. Although, I am less experienced my thirst for knowledge has helped me through a brutal 24 week coding bootcamp through the University of Utah with high marks.   </h2>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default AboutMe;
